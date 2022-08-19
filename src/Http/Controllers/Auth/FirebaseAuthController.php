39ee7a798e98069cb95200fd7dec0809411d8257
Luke Siedle <ljsiedle@gmail.com>
2022-08-19 11:49:40 +0100

    Updates the controller with a json-only verification method

--- src/Http/Controllers/Auth/FirebaseAuthController.php
+++ src/Http/Controllers/Auth/FirebaseAuthController.php
@@ -19,18 +19,15 @@ use Salt\Firebase\Services\AuthService;
  */
 class FirebaseAuthController extends Controller
 {
-    public function __construct(AuthService $auth)
+    public function __construct(AuthService $firebase_auth_service)
     {
-        $this->auth = $auth;
-        $this->login_url = route(config('salt-firebase.routes.login'));
-        $this->login_success_url = route(config('salt-firebase.routes.login_success'));
-        $this->error_url = route(config('salt-firebase.routes.login_error'));
+        $this->firebase_auth_service = $firebase_auth_service;
     }
 
-    public function index()
+    public function index( Request $request  )
     {
         if (! Auth::check()) {
-            return redirect()->to($this->login_url);
+            return redirect()->to(route(config('salt-firebase.routes.login')));
         }
     }
 
@@ -42,37 +39,60 @@ class FirebaseAuthController extends Controller
     public function verifySSOToken(Request $request)
     {
         if (Auth::check()) {
-            return redirect()->to($this->login_success_url);
+            return redirect()->to(route(config('salt-firebase.routes.login_success')));
         }
 
         // Check token exists
         if (! $request->input('token')) {
-            return redirect()->to($this->error_url);
+            return redirect()->to(route(config('salt-firebase.routes.login_error')))->with('error', 'Token not provided');
         }
 
         // Log the user in or redirect to login
         try {
             if ($request->get('reg')) {
-                $this->auth->processSignUpFromToken($request->input('token'));
+                $this->firebase_auth_service->processSignUpFromToken($request->input('token'));
             } else {
-                $this->auth->processLoginFromToken($request->input('token'));
+                $this->firebase_auth_service->processLoginFromToken($request->input('token'));
             }
         } catch (AuthServiceException $e) {
-            return redirect()->to($this->error_url);
+            return redirect()->to(route(config('salt-firebase.routes.login_error')))->with('error', $e->getMessage());
         }
 
-        return redirect()->to($this->login_success_url);
+        return redirect()->to(route(config('salt-firebase.routes.login_success')));
+    }
+
+    /**
+     * Checks that a token is still valid
+     */
+    public function verifySSOTokenInBackground(Request $request)
+    {
+        // Check token has been passed
+        if (! $request->input('token')) {
+            return response()->json([
+                'error' => 'Token not provided'
+            ], 400);
+        }
+
+        try {
+            $this->firebase_auth_service->processLoginFromToken($request->input('token'));
+        } catch (AuthServiceException $e) {
+            return response()->json([
+                'error' => 'Could not resume the session.'
+            ], 403);
+        }
+
+        return response()->json();
     }
 
     /**
      * Logs the user out and redirects
      * to login url.
      */
-    public function logout()
+    public function logout( Request $request )
     {
-        $this->auth->logout();
+        $this->firebase_auth_service->logout();
 
-        return redirect()->to($this->login_url);
+        return redirect()->to(route(config('salt-firebase.routes.login')));
     }
 
     /**
@@ -80,7 +100,7 @@ class FirebaseAuthController extends Controller
      * using a different view to display
      * the error.
      */
-    public function error()
+    public function error( Request $request )
     {
         return response()->json([
             'error_message' => 'Sorry there was an issue logging you in, please try again or contact support',

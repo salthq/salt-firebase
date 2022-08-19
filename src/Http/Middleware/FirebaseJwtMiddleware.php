75eed3662980503b2d7f158973e70a304170c029
Luke Siedle <ljsiedle@gmail.com>
2022-08-19 11:50:09 +0100

    Make middleware more configurable

--- src/Http/Middleware/FirebaseJwtMiddleware.php
+++ src/Http/Middleware/FirebaseJwtMiddleware.php
@@ -3,6 +3,7 @@
 namespace Salt\Firebase\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Salt\Firebase\Services\AuthService;
 
@@ -17,10 +18,14 @@ class FirebaseJwtMiddleware
      * Check if the user's JWT has expired
      * if the user is currently signed in.
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
+        $redirect_url = route(config('salt-firebase.routes.login_refresh'));
+
         if (Auth::check() && ! $this->auth->sessionTokenIsValid()) {
             $this->auth->logout();
+            $request->session()->flash('redirect_url', $request->url());
+            return redirect($redirect_url);
         }
 
         return $next($request);
